d71621e804ab529974224e11bd1d7f21b25bb686
Bryant Derosier <bryant.derosier@firsthorizon.com>
2026-09-03 11:39:34 -0400

    feat(client): show built-in agents in settings

--- apps/web/src/components/ChatView.tsx
+++ apps/web/src/components/ChatView.tsx
@@ -8576,6 +8576,12 @@ export default function ChatView(props: ChatViewProps) {
                             threadSyncPhase={activeEnvironmentUnavailable ? null : threadSyncPhase}
                             runtimeMode={runtimeMode}
                             interactionMode={interactionMode}
+                            {...(serverProjection?.thread.agentPersonaAssignment === undefined
+                              ? {}
+                              : {
+                                  agentPersonaAssignment:
+                                    serverProjection.thread.agentPersonaAssignment,
+                                })}
                             lockedProvider={modelPickerLockedProvider}
                             providerStatuses={providerStatuses as ServerProvider[]}
                             activeProjectDefaultModelSelection={
