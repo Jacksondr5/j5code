5a22f8d5a48b31dbe071739dea94171e5af243ec
Bastian Huppertz <bastian.huppertz@firsthorizon.com>
2026-09-24 19:37:59 +0200

    refactor(mobile): reuse playbook workspace selector

--- apps/mobile/src/j5/playbooks/workspaceInputs.ts
+++ apps/mobile/src/j5/playbooks/workspaceInputs.ts
@@ -1,3 +1,4 @@
+import { samePlaybookWorkspaceInputs } from "@t3tools/client-runtime/j5/playbooks";
 import { Atom } from "effect/unstable/reactivity";
 import { environmentProjects } from "../../state/projects";
 import { environmentThreadShells } from "../../state/threads";
@@ -11,27 +12,5 @@ const workspaceInputsAtom = Atom.make((get) => ({
 
 export const playbookWorkspaceInputsAtom = Atom.withEquality(
   workspaceInputsAtom,
-  (a, b) =>
-    a.projects.length === b.projects.length &&
-    a.threads.length === b.threads.length &&
-    a.projects.every((project, index) => {
-      const previous = b.projects[index];
-      return (
-        project.environmentId === previous?.environmentId &&
-        project.id === previous.id &&
-        project.title === previous.title &&
-        project.workspaceRoot === previous.workspaceRoot
-      );
-    }) &&
-    a.threads.every((thread, index) => {
-      const previous = b.threads[index];
-      return (
-        thread.environmentId === previous?.environmentId &&
-        thread.id === previous.id &&
-        thread.projectId === previous.projectId &&
-        thread.title === previous.title &&
-        thread.worktreePath === previous.worktreePath &&
-        thread.branch === previous.branch
-      );
-    }),
+  samePlaybookWorkspaceInputs,
 ).pipe(Atom.withLabel("mobile-j5:playbook-workspaces"));
